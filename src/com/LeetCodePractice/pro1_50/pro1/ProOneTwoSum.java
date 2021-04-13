72086982f7e6c5856d361066ee897ad542f5ed5e
Kou <kou1688@foxmail.com>
2021-04-13 20:17:23 +0800

    LeetCodeKey

---
+++ src/com/LeetCodePractice/pro1_50/pro1/ProOneTwoSum.java
@@ -0,0 +1,55 @@
+package com.LeetCodePractice.pro1_50.pro1;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 给定一个整数数组 nums和一个整数目标值 target，请你在该数组中找出 和为目标值
+ * 的那两个整数，并返回它们的数组下标。
+ * <p>
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * <p>
+ * 你可以按任意顺序返回答案。
+ * <p>
+ * 示例 1：
+ * <p>
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[0,1]
+ * 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/two-sum
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class ProOneTwoSum {
+    /**
+     * 在数组中找到和为target的两个数
+     *
+     * @param nums   数组
+     * @param target 目标和
+     * @return 返回一个装有结果下标的数组
+     */
+    public int[] twoSum(int[] nums, int target) {
+        //暴力遍历
+        /*int[] arr = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    arr[0] = i;
+                    arr[1] = j;
+                    break;
+                }
+            }
+        }
+        return arr;*/
+        //哈希表
+        Map<Integer, Integer> numHash = new HashMap<>();    //key存储数值,value存储下标
+        for (int i = 0; i < nums.length; i++) {
+            if (numHash.containsKey(target - nums[i])) {    //找到符合条件的值
+                return new int[]{numHash.get(target - nums[i]), i}; //将符合条件的value值与当前下标存入数组返回
+            }
+            numHash.put(nums[i], i);    //没有符合条件的key,将当前数值和下标存入散列表
+        }
+        throw new IllegalArgumentException("Could not find");   //没有找到,抛出异常
+    }
+}
---
+++ src/com/LeetCodePractice/pro1_50/pro2/Pro2AddTwoNumbers.java
@@ -0,0 +1,42 @@
+package com.LeetCodePractice.pro1_50.pro2;
+
+public class Pro2AddTwoNumbers {
+    /**
+     * 两数相加
+     *
+     * @param l1 第一个数(链表逆序形式)
+     * @param l2 第二个数(链表逆序形式)
+     * @return 返回结果(链表)
+     */
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode head = null; //头结点,用于标记结果链表
+        ListNode last = null; //尾结点,尾结点没有上一个结点的信息
+        int carry = 0;    //用来保存计算产生的进位值
+
+        while (l1 != null || l2 != null) {
+            //若l1不为空取val值,否则补零
+            int num1 = (l1 != null) ? l1.val : 0;
+            int num2 = (l2 != null) ? l2.val : 0;
+            int sumVal = num1 + num2 + carry;
+            if (head == null) {    //第一次取值,需要给头结点值,以标记链表
+                head = last = new ListNode(sumVal % 10);
+            } else {
+                last.next = new ListNode(sumVal % 10);   //因为每个结点只能存储一位数字,所以需要取余
+                last = last.next;   //让last指向当前非空最后一个结点
+            }
+            carry = sumVal / 10;    //计算进位
+            //遍历链表
+            if (l1 != null) {
+                l1 = l1.next;
+            }
+            if (l2 != null) {
+                l2 = l2.next;
+            }
+        }
+
+        if (carry > 0) {    //若有进位,补一个结点
+            last.next = new ListNode(carry);
+        }
+        return head;
+    }
+}
